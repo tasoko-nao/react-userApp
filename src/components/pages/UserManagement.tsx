f8d679e7ad351ac8622ca14019302f26a9a750a8
tasoko-nao <tsk0107@gmail.com>
2022-07-28 17:15:59 +0900

    07/28

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,17 +1,60 @@
-import { Wrap, WrapItem } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import {
+  Center,
+  Spinner,
+  useDisclosure,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
+import { memo, useCallback, useEffect, VFC } from "react";
+import { useAllUsers } from "../../hooks/useAllUsers";
+import { useLoginUser } from "../../hooks/useLoginUser";
+import { useSelectUser } from "../../hooks/useSelectUser";
 import { UserCard } from "../organisms/user/UserCard";
+import { UserDetailModal } from "../organisms/user/UserDetailModal";
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, loading, users } = useAllUsers();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { selectedUser, onSelectUser } = useSelectUser();
+
+  const { loginUser } = useLoginUser();
+
+  const onClickUser = useCallback(
+    (id: number) => {
+      onSelectUser({ id, users, onOpen });
+    },
+    [users]
+  );
+
+  useEffect(() => getUsers(), []);
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://source.unsplash.com/random"
-          userName="taso"
-          fullName="tasoko naoya"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                id={user.id}
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+                onClickOpen={onClickUser}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+      <UserDetailModal
+        isOpen={isOpen}
+        isAdmin={loginUser?.isAdmin}
+        onClose={onClose}
+        user={selectedUser}
+      />
+    </>
   );
 });
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,28 +2,33 @@ import axios from "axios"
 import { useCallback, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { User } from "../types/api/user";
+import { useLoginUser } from "./useLoginUser";
 import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const history = useHistory();
   const [loading, setLoading] = useState(false);
   const { showMessage } = useMessage();
+
+  const { setLoginUser } = useLoginUser()
+
   const login = useCallback((id: string) => {
     setLoading(true)
     axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`).then((res) => {
       if(res.data){
+        const isAdmin = res.data.id === 10 ? true: false
+        setLoginUser({...res.data, isAdmin})
         showMessage({title: "ログインしました", status: "success"})
         history.push("/home")
       } else {
         showMessage({title: "ユーザーが見つかりません。", status: "error"})
+        setLoading(false)
       }
     })
     .catch(() => {
       showMessage({title: "ログインできません。", status: "error"})
-    })
-    .finally(() => {
       setLoading(false)
     })
-  }, [history, showMessage])
+  }, [history, showMessage, setLoginUser])
   return { login, loading }
 }
